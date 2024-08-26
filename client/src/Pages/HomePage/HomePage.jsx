c8dc5b12287ef24c206e0f7f0b0e71e5d51e985b
emanueldavidov <130387402+emanueldavidov@users.noreply.github.com>
2024-08-26 12:56:07 +0300

    fixed writing colors

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -7,76 +7,97 @@ import { BACKEND_URL } from '../../config/config'
 
 
 const HomePage = () => {
-const dispatch = useDispatch();
-const userID = useSelector((state) => state.auth.user._id);
-const recipes = useSelector((state) => state.recipes.recipes);
-const meals = useSelector((state) => state.meals.meals);
+  const dispatch = useDispatch();
+  const userID = useSelector((state) => state.auth.user._id);
+  const recipes = useSelector((state) => state.recipes.recipes);
+  const meals = useSelector((state) => state.meals.meals);
 
- useEffect(() => {
-  const fetchRecipes = async () => {
-    try {
-      const response = await fetch( `${BACKEND_URL}/api/recipe/recipes`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch recipes');
-      }
-      const data = await response.json();
-      dispatch(setRecipes(data));
-    } catch (error) {
-      console.error('Error fetching recipes:', error);
-    }
-  };
-
-  const fetchMeals = async () => {
-    try {
-      const response = await fetch(`${BACKEND_URL}/api/meal/${userID}/meals`);
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch meals');
-      }
-      const data = await response.json();
-      dispatch(setMeals(data));
-    } catch (error) {
-      console.error('Error fetching meals:', error);
-    }
-  }
-
-
-  fetchRecipes();
-  fetchMeals();
-}, []);
-const user=useSelector((state)=>state.auth.user)
-    const data = [
-        {
-          src: 'make-a-recipe.png',
-          title: 'Share a Recipe!',
-          link:"/recipe"
-        },
-        {
-          src: 'plan-a-meal.png',
-          title: 'Plan a Meal!',
-          link:"/meal"
-
-        },
-        {
-          src: 'shopping-list.png',
-          title: 'Shopping List!',
-          link:"/shopping"
-        },
-        {
-            src: 'nutrition-calc.png',
-            title: 'Nutritional Calculator!',
-            link:"/nutrition"
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch(`${BACKEND_URL}/api/recipe/recipes`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch recipes');
         }
-      ];
+        const data = await response.json();
+        dispatch(setRecipes(data));
+      } catch (error) {
+        console.error('Error fetching recipes:', error);
+      }
+    };
+
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(`${BACKEND_URL}/api/meal/${userID}/meals`);
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch meals');
+        }
+        const data = await response.json();
+        dispatch(setMeals(data));
+      } catch (error) {
+        console.error('Error fetching meals:', error);
+      }
+    }
+
+
+    fetchRecipes();
+    fetchMeals();
+  }, []);
+  const user = useSelector((state) => state.auth.user)
+  const isDarkMode = useSelector((state) => state.darkMode.darkMode);
+
+const data = [
+    {
+      src: 'make-a-recipe.png',
+      title: (
+        <div className={`rounded inline-block ${isDarkMode ? 'bg-[#535353] text-white' : 'bg-white text-black'}`}>
+          Share a Recipe!
+        </div>
+      ),
+      link: "/recipe"
+    },
+    {
+      src: 'plan-a-meal.png',
+      title: (
+        <div className={`rounded inline-block ${isDarkMode ? 'bg-[#535353] text-white' : 'bg-white text-black'}`}>
+          Plan a Meal!
+        </div>
+      ),
+      link: "/meal"
+    },
+    {
+      src: 'shopping-list.png',
+      title: (
+        <div className={`rounded inline-block ${isDarkMode ? 'bg-[#535353] text-white' : 'bg-white text-black'}`}>
+          Shopping List!
+        </div>
+      ),
+      link: "/shopping"
+    },
+    {
+        src: 'nutrition-calc.png',
+        title: (
+          <div className={`rounded inline-block ${isDarkMode ? 'bg-[#535353] text-white' : 'bg-white text-black'}`}>
+            Nutritional Calculator!
+          </div>
+        ),
+        link: "/nutrition"
+    }
+];
+
+
+
+
   return (
     <>
-     <div className="flex justify-center mt-5 font-bold">
+      <div className="flex justify-center mt-5 font-bold">
         Hello {user.username}!
       </div>
       <div className="mb-2.5 mt-2.5 flex justify-center">
         <img className="max-w-[100px] h-auto" src="/logo.png" alt="Logo" />
       </div>
-    <CustomMedia data={data}/>
+      <CustomMedia data={data} />
     </>
   )
 }
